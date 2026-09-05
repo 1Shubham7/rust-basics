59c29c1d4cb2086dc92ef72c7d307148d457bd73
Shubham Singh <shubhammahar1306@gmail.com>
2026-09-05 14:27:45 +0530

    hello world program
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

---
+++ hello/target/debug/deps/hello-4fa869b2b2dd0b40.d
@@ -0,0 +1,5 @@
+/home/shubham/Code/Personal/rust-basics/hello/target/debug/deps/hello-4fa869b2b2dd0b40.d: src/main.rs
+
+/home/shubham/Code/Personal/rust-basics/hello/target/debug/deps/hello-4fa869b2b2dd0b40: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/shubham/Code/Personal/rust-basics/hello/target/debug/hello: /home/shubham/Code/Personal/rust-basics/hello/src/main.rs
